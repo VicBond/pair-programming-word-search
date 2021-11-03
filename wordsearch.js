ebd102c93b48afc8b0032335cf2c07f5310af269
Victor Bondarenko <nirasimha@gmail.com>
2021-11-03 22:06:32 +0000

    Word-search with test file added

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  const transposedLetters = transpose(letters);
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transposedLetters.map(ls => ls.join(''));
+  
+  
+  if (letters === []) {
+    return false;
+  }
+  for (let h of horizontalJoin) {
+    if (h.includes(word)) return true;
+  }
+  for (let v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+  return false;
+};
+
+const transpose = function(matrix) {
+  
+  let newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix[i] = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newMatrix[i][j] = matrix[j][i];
+    }
+  }
+  return newMatrix;
+};
+  
+
+module.exports = wordSearch;
